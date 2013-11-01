860d1d0235136d58058c57fb81283e3081396942
Paul Williams <paulswilliams@me.com>
2013-11-01 19:26:57 +0000

    initial structure

---
+++ src/main/java/uk/me/paulswilliams/projecteuler/problem1/App.java
@@ -0,0 +1,13 @@
+package uk.me.paulswilliams.projecteuler.problem1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
